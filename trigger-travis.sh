a9573bcd936e0c9c15d0cdd174cc8acb7a77f532
Alessandro Jean <alessandrojean@gmail.com>
2019-11-25 00:17:01 -0300

    Change behavior of trigger.

--- trigger-travis.sh
+++ trigger-travis.sh
@@ -11,19 +11,24 @@ body="{
   }
 }"
 
-curl -s -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" \
-  -H "Authorization: token ${TRAVIS_TOKEN}" \
-  -d "$body" \
-  https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests \
-    | tee /tmp/travis-request-output.txt
+# Only triggers if the data has changed.
+if git diff --name-only HEAD~1 HEAD | grep database.sqlite; then
+  curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${TRAVIS_TOKEN}" \
+    -d "$body" \
+    https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests \
+      | tee /tmp/travis-request-output.txt
 
-if grep -q '"@type": "error"' /tmp/travis-request-output.txt; then
-  exit 1
-fi
+  if grep -q '"@type": "error"' /tmp/travis-request-output.txt; then
+    exit 1
+  fi
 
-if grep -q 'access denied' /tmp/travis-request-output.txt; then
-  exit 1
+  if grep -q 'access denied' /tmp/travis-request-output.txt; then
+    exit 1
+  fi
+else
+  echo "Data has not changed, so the build was not triggered."
 fi
